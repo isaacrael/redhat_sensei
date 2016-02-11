62cd46204dfb5320806facfdb3355981090090cc
isaacrael <grael@comcast.net>
2016-02-10 20:13:14 -0700

    added question and answer models

--- redhat_sensei_quiz/models.py
+++ redhat_sensei_quiz/models.py
@@ -1,3 +1,32 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+    category = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    answer_text = models.CharField(max_length=200)
+    answers = models.IntegerField(default=0)
+    correct_answer = models.CharField(max_length=200, default='Linus Torvalds')
+
+
+    def __unicode__(self):
+        return self.answer_text
+
